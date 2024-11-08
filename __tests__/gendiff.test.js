f7ff3804bfcf0a872e30e19c98871093d2eda9e5
vladsus <you@example.com>
2024-11-08 13:00:44 +0000

    add fixtures and modified test for use fixtures

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,20 +1,21 @@
+import fs from 'fs';
 import genDiff from '../gendiff.js';
 
 test('gendiff should compare two JSON objects and produce the correct output', () => {
-  const data1 = { a: 1, b: 2, c: 3 };
-  const data2 = { a: 1, b: 3, d: 4 };
+  const dataFilePath = '/home/vladsus/frontend-project-46/__fixtures__/data.json';
+  const expectedOutputFilePath = '/home/vladsus/frontend-project-46/__fixtures__/expectedOutput.json';
 
-  const expectedOutput = `{
-    - b: 2
-    + b: 3
-      a: 1
-    + d: 4
-    - c: 3
-  }`;
+  const dataFileContent = fs.readFileSync(dataFilePath, 'utf-8');
+  const { data1, data2 } = JSON.parse(dataFileContent);
+
+  const expectedOutputContent = fs.readFileSync(
+    expectedOutputFilePath,
+    'utf-8',
+  );
+  const { output: expectedOutput } = JSON.parse(expectedOutputContent);
 
   const result = genDiff(data1, data2);
 
-  // Normalizar la cadena: eliminar saltos de línea adicionales y ordenar las líneas
   const normalizeString = (str) => str
     .split('\n') // Divide en líneas
     .map((line) => line.trim()) // Elimina los espacios adicionales de cada línea
